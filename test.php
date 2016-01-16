057ae8e7c168e1d43f81a9222e80306887964fa4
Eugen <eugen.bogdanovich@gmail.com>
2016-01-16 13:19:52 +0000

    first commit

---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eugen
+ * Date: 16.01.2016
+ * Time: 11:37
+ */
+
+require_once('google-url-master/autoload.php');
+
+        $googleUrl=new \GoogleUrl();
+        $googleUrl->setLang('en') // lang allows to adapt the query (tld, and google local params)
+        ->setNumberResults(10);                        // 10 results per page
+        $acdcPage1=$googleUrl->setPage(0)->search("acdc"); // acdc results page 1 (results 1-10)
+        $acdcPage2=$googleUrl->setPage(1)->search("acdc"); // acdc results page 2 (results 11-20)
+
+        $googleUrl->setNumberResults(20);
+        $simpsonPage1=$googleUrl->setPage(0)->search("simpson"); // simpsons results page 1 (results 1-20)
+
+
+
+
+        // GET NATURAL RESULTS
+
+        $positions=$simpsonPage1->getPositions();
+
+        echo "results for " . $simpsonPage1->getKeywords();
+        echo "<ul>";
+        foreach($positions as $result){
+            echo "<li>";
+            echo "<ul>";
+            echo "<li>position : " . $result->getPosition() . "</li>";
+            echo "<li>title : "    . utf8_decode($result->getTitle())    . "</li>";
+            echo "<li>website : "  . $result->getWebsite()  . "</li>";
+            echo "<li>URL : <a href='" . $result->getUrl() ."'>" . $result->getUrl() . "</a></li>";
+            echo "</ul>";
+            echo "</li>";
+        }
+        echo "</ul>";
